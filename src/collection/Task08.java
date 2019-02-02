b3acc819b802bc72813fc1f93509d4f3806e23aa
Damir <rdamir82@gmail.com>
2019-02-02 12:51:10 +0100

    collections

---
+++ src/collection/Task08.java
@@ -0,0 +1,32 @@
+package collection;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Task08 {
+
+	public static void main(String[] args) {
+		
+		// Write a Java program to sort a given array list.
+		
+		ArrayList<String> list = new ArrayList<>();
+		
+		list.add("Hello");
+		list.add("There");
+		list.add("How");
+		list.add("Are");
+		list.add("You");
+		list.add("?");
+		
+		System.out.println(list);
+		
+		Collections.sort(list);
+		
+		System.out.println(list);
+		
+		Collections.reverse(list);
+		
+		System.out.println(list);
+	}
+
+}
---
+++ src/collection/Task09.java
@@ -0,0 +1,45 @@
+package collection;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Task09 {
+
+	public static void main(String[] args) {
+		
+		// Write a Java program to copy one array list into another
+	
+		ArrayList<String> list = new ArrayList<>();
+		
+		list.add("Pencil");
+		list.add("Sheet");
+		
+		System.out.println("List = " + list);
+		
+		ArrayList<String> list2 = new ArrayList<>();
+		
+		list2.add("Table");
+		list2.add("Something");
+		
+		System.out.println("List2 = " + list2);
+		
+		// this is adding, NOT COPY! 
+		
+		//list2.addAll(list);
+		
+		//System.out.println("List2 after adding list -> List2 = " + list2);
+		
+		// Solution:
+		
+		Collections.copy(list, list2);
+		
+		System.out.println("List = " + list);
+		
+		System.out.println("List2 = " + list2);
+		
+	
+	
+	
+	}
+
+}
