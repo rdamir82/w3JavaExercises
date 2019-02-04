d7f7b8ddb3e7156167d444676dfba6acf95ecdce
Damir <rdamir82@gmail.com>
2019-02-04 07:20:46 +0100

    LinkedList

---
+++ src/collectionLinkedList/task01appentToTheEnd.java
@@ -0,0 +1,22 @@
+package collectionLinkedList;
+
+import java.util.LinkedList;
+
+public class task01appentToTheEnd {
+
+	public static void main(String[] args) {
+		
+		// Write a Java program to append the specified element to the end of a linked list
+		
+		LinkedList<String> list = new LinkedList<>();
+		
+		list.add("Hello");
+		list.add("There");
+		list.add("How");
+		list.add("Are");
+		list.add("You");
+		
+		System.out.println(list);
+	}
+
+}
